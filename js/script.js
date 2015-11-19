fec0cbe87bf1976a7e137f09f564332a6a6a9d8e
jamescha <james.cha.zero@gmail.com>
2015-11-18 20:39:56 -0800

    Fixed card width and center p

--- js/script.js
+++ js/script.js
@@ -65,7 +65,8 @@ $(document).ready(function(){
   //Fill the div with all the messages
   myFirebaseRef.on("child_added", function(snapshot, prevChildKey) {
     var value = snapshot.val();
-    $("#messages").append('<paper-card> <p class="message">' + value["userName"]+ ' Says: ' + value["text"] + '</p> </paper-card> <br>');
+    $("#messages").append('<paper-card class="paperCard"> <p class="message">' +
+     value["userName"] + ' Says: ' + value["text"] + '</p> </paper-card> <br>');
   });
 
   //Log in user for Firebase with email and password Auth built into Firebase
